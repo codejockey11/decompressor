8e25c5bae48ce82f0519f0d5b4584f76aa306ba1
codejockey11 <junk_1105@hotmail.com>
2024-02-04 19:47:59 -0500

    Master Commit

---
+++ decompressor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace decompressor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String userprofileFolder = Environment.GetEnvironmentVariable("USERPROFILE");
+            String[] fileEntries = Directory.GetFiles(userprofileFolder + "\\Downloads\\", "28DaySubscription*.zip");
+
+            ZipArchive archive = ZipFile.OpenRead(fileEntries[0]);
+            ZipArchiveEntry entry = archive.GetEntry(args[0]);
+            entry.ExtractToFile(args[0], true);
+        }
+    }
+}
